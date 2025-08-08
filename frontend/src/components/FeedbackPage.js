5264b49b2fbfa93815f98830526e5ff6bdbfebb2
David Steblina <abc@gmail.com>
2025-08-08 04:44:16 +0000

    auto-commit for de1479da-a3d6-41ff-a45f-f7b88a6905d6

--- frontend/src/components/FeedbackPage.js
+++ frontend/src/components/FeedbackPage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
 import { Badge } from "./ui/badge";
+import PageHeader from "./PageHeader";
 import { 
   MessageSquare, 
   Star, 
